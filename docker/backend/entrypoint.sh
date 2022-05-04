289f5a081dd68409123985788a810df9c5101c1a
Jiahui LUO <41533725+tfyd@users.noreply.github.com>
2022-05-04 11:51:58 +0800

    rename docker

--- docker/backend/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd /CCC_Project2/Backend/
-python3 -m pip install -r requirements.txt
-python3 --version
-python3 api.py
-exec "$@"
--- docker/harvest/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd /CCC_Project2/TwitterStreamer/
-python3 -m pip install -r stream_requirements.txt
-python3 --version
-python3 tweetStreamer.py
-exec "$@"
