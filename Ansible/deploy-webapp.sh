b825ab71e64915364e84f7dcadf5820a771aed48
tfyd <z5158415@student.unsw.edu.au>
2022-05-08 14:26:34 +0800

    update ansible to use builtin docker compose

--- Ansible/deploy-webapp.sh
+++ Ansible/deploy-webapp.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-ansible-playbook -i inventory/hosts.ini -u ubuntu --key-file=/Users/gray/group-8.pem webapp.yaml
+# ansible-playbook -i inventory/hosts.ini -u ubuntu --key-file=/Users/gray/group-8.pem webapp.yaml
+
+ansible-playbook -i inventory/hosts.ini -u ubuntu --key-file=/Users/QAQ/ccc-project/group-8.pem webapp.yaml
