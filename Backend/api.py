82e47c02149cd752e2c082f39fba4773b7cc0e98
tfyd <z5158415@student.unsw.edu.au>
2022-05-09 11:32:30 +0800

    add aurin backend

--- Backend/api.py
+++ Backend/api.py
@@ -1,3 +1,4 @@
+from aurin import AurinRai, AurinSeat
 from flask_restful import Api, Resource, reqparse
 from flask import Flask, jsonify
 from tweets import SentimentCount, Tweets
@@ -7,6 +8,8 @@ api = Api(app)
 
 api.add_resource(Tweets, '/tweets/<keyword>')
 api.add_resource(SentimentCount, '/sentiment/<keyword>')
+api.add_resource(AurinRai, '/aurin/rai/<year>')
+api.add_resource(AurinSeat, '/aurin/seat')
 api.init_app(app)
 
 app.run(host='0.0.0.0', port=5555, use_reloader=True)
---
+++ Backend/aurin.py
@@ -0,0 +1,38 @@
+from flask_restful import Api, Resource, reqparse
+from flask import Flask, jsonify
+from config import remote_server, DB_URI
+import requests
+import numpy as np
+
+class AurinRai(Resource):
+
+    def get(self, year):
+        self.db = remote_server['aurin_rai']
+        res = {}
+        count = []
+        for item in self.db.view('_all_docs', include_docs=True):
+            doc = item.doc
+            res[doc['suburb']] = doc[str(year)]
+            count.append(doc[str(year)])
+        res['OVERALL'] = {'min': np.min(count), 'max': np.max(count), "mean": np.mean(count)}
+        
+        response = jsonify(res)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+class AurinSeat(Resource):
+
+    def get(self):
+        self.db = remote_server['aurin_seat']
+        res = {}
+        count = []
+        for item in self.db.view('_all_docs', include_docs=True):
+            doc = item.doc
+            res[doc['suburb']] = doc['seat']
+            count.append(doc['seat'])
+        res['OVERALL'] = {'min': float(np.min(count)), 'max': float(np.max(count)), "mean": float(np.mean(count))}
+        print(res)
+   
+        response = jsonify(res)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
