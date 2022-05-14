ee082f17d0497647a751259ae9f7d143e709d37e
yebinge <yebing@student.unimelb.edu.au>
2022-05-14 22:47:10 +0800

    Change key file directory

--- Ansible/git-pull.sh
+++ Ansible/git-pull.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# ansible-playbook -i inventory/hosts.ini -u ubuntu --key-file=/Users/gray/group-8.pem git-pull.yaml
-ansible-playbook -i inventory/hosts.ini -u ubuntu --key-file=/Users/QAQ/ccc-project/group-8.pem git-pull.yaml
+ ansible-playbook -i inventory/hosts.ini -u ubuntu --key-file=/Users/gray/group-8.pem git-pull.yaml
+#ansible-playbook -i inventory/hosts.ini -u ubuntu --key-file=/Users/QAQ/ccc-project/group-8.pem git-pull.yaml
